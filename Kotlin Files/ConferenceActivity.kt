038573471171f75dc1af9f94fcc4b145fbe29fb9
JungHoiMin <76022353+JungHoiMin@users.noreply.github.com>
2021-07-31 10:18:20 +0900

    Add files via upload

---
+++ Kotlin Files/ConferenceActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.veritas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_conference)
+    }
+}
